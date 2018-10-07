193c673375ca84afbfcc0bdfe67d8ffa4b480d5f
HauserDavid <hauser.david@hotmail.com>
2018-10-07 14:43:43 +0200

    01_10_2018_Module

---
+++ assignment1/app/hallo.js
@@ -0,0 +1,8 @@
+
+let data = {
+    name: function (prefix) {
+        return prefix + "Welt";
+    }
+};
+
+module.exports = data;
---
+++ assignment1/app/unterordner/index.js
@@ -0,0 +1,3 @@
+console.log("ich bin eunterordner/index.js");
+
+require("./app.js");
