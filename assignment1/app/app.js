51385b0149aacdfca9c3d5b8de6588b7319aa621
HauserDavid <hauser.david@hotmail.com>
2018-11-06 17:01:53 +0100

    ValueType und ReferenceType

--- assignment1/app/app.js
+++ assignment1/app/app.js
@@ -106,7 +106,7 @@ console.log('total = ', total, total.constructor);*/
 
 //Pass-by-Value, Pass-by-Reference
 
-let hallo = function (str) {
+/*let hallo = function (str) {
     str = "Hallo + " + str;
 };
 
@@ -125,4 +125,51 @@ let o = {
 
 halloObject(o);
 
-console.log(o);
+console.log(o);*/
+
+
+//Value Type
+
+/*let x = 10;
+let y = x;
+
+x = 20;
+
+console.log("x =", x);
+console.log("y = ", y);*/
+
+
+//Reference Type
+
+/*let x = {value: 10};
+let y = x;
+
+x.value = 20;
+
+console.log("x =", x);
+console.log("y =", y);*/
+
+
+//Value Type
+
+
+/*let number = 10;
+
+function increase(number){
+    number++;
+}
+
+increase(number);
+console.log(number);*/
+
+
+//Reference Type
+
+/*let obj = {value: 10};
+
+function increase(obj) {
+    obj.value++;
+}
+
+increase(obj);
+console.log(obj);*/
